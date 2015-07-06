ce605c668b0a65d131b005b76c6173ebf7f9381b
Jake Lane <me@jakelane.me>
2015-07-06 13:09:42 +0930

    Changed to use inbox

--- bot.py
+++ bot.py
@@ -29,30 +29,23 @@ def generateConfig():
 	print('Configuration not found, a default config.cfg was generated (you must edit it)')
 	sys.exit()
 
-def generate_comment(r, comment, already_done, query, wolframclient):
-	comment_with_replies = r.get_submission(comment.permalink).comments[0]
-	for reply in comment_with_replies.replies:
-		if reply.author.name == 'WolframAlpha-Bot':
-			already_done.add(comment.id)
-			print('Comment was already done.')
-			break
-	if comment.id not in already_done:
-		# The porridge is feeling alright
-		comment_reply = ''
-		print('Processing comment')
-		for formula in query:
-			res = wolframclient.query(formula)  # Query the api
-			for pod in res.pods:
-				if pod.text: # If there is plaintext
-					comment_reply = comment_reply + '**' + pod.title + '**\n\n\t' + pod.text + '\n\n'
-				elif pod.main.node.find('img').get('src'): # Try and print an image if it exists
-					comment_reply = comment_reply + '**' + pod.title + '**\n\n[Image](' + pod.main.node.find('img').get('src') + ')\n\n'
-				# Otherwise we pretend nothing was found (as there was no output we can use for this pod)
-		
-		comment_reply = comment_reply + '***\n[^About](https://github.com/JakeLane/WolframAlpha-Reddit-Bot) ^| [^(Report a Bug)](https://github.com/JakeLane/WolframAlpha-Reddit-Bot/issues) ^(| Created and maintained by /u/JakeLane)'
-		comment.reply(comment_reply)
-		already_done.add(comment.id)
-		print('Successfully posted comment.')
+def generate_comment(comment, query):
+	# The porridge is feeling alright
+	comment_reply = ''
+	print('Processing comment')
+	for formula in query:
+		res = wolframclient.query(formula)  # Query the api
+		for pod in res.pods:
+			if pod.text: # If there is plaintext
+				comment_reply = comment_reply + '**' + pod.title + '**\n\n\t' + pod.text + '\n\n'
+			elif pod.main.node.find('img').get('src'): # Try and print an image if it exists
+				comment_reply = comment_reply + '**' + pod.title + '**\n\n[Image](' + pod.main.node.find('img').get('src') + ')\n\n'
+			# Otherwise we pretend nothing was found (as there was no output we can use for this pod)
+	
+	comment_reply = comment_reply + '***\n[^About](https://github.com/JakeLane/WolframAlpha-Reddit-Bot) ^| [^(Report a Bug)](https://github.com/JakeLane/WolframAlpha-Reddit-Bot/issues) ^(| Created and maintained by /u/JakeLane)'
+	comment.reply(comment_reply)
+	comment.mark_as_read()
+	print('Successfully posted comment.')
 
 def main():
 	# Read the config
@@ -68,39 +61,36 @@ def main():
 		generateConfig()
 	
 	# OAuth and reddit initialisation
-	r = praw.Reddit('WolframAlpha script by /u/JakeLane'
-					'Url: https://github.com/JakeLane/WolframAlpha-Reddit-Bot')
+	global r
+	r = praw.Reddit('WolframAlpha script by /u/JakeLane')
 	r.config.decode_html_entities = True
 	o = OAuth2Util.OAuth2Util(r)
 	o.refresh()
-	
+
 	# Create wolframalpha
+	global wolframclient
 	wolframclient = wolframalpha.Client(app_id)
 	
 	# Define the regex
 	regex = re.compile(r'\[(.*)\]\(\/u\/WolframAlpha-Bot\)', re.I)
 	
-	already_done = set()
-
 	print('WolframAlpha-Bot is now running')
+	# Start the main loop
 	while True:
-		try:
-			all_comments = praw.helpers.comment_stream(r, 'all', limit=None, verbosity=0)
-			
-			# Start the main loop
-			while True:
-				for comment in all_comments:
-					query = []
-					query.extend(regex.findall(comment.body))
-					if query != [] and comment.id not in already_done:
-						try:
-							print('Found comment with query')
-							generate_comment(r, comment, already_done, query, wolframclient)
-						except HTTPError as e:
-							print('HTTPError: Most likely banned')
-	
-		except Exception as e:
-			print(e)
-			continue
+		print("Checking inbox")
+		messages = r.get_unread()
+		for comment in messages:
+			query = []
+			query.extend(regex.findall(comment.body))
+			if query != [] and comment.id:
+				try:
+					print('Found comment with query')
+					generate_comment(comment, query)
+				except HTTPError as e:
+					print('HTTPError: Most likely banned')
+
+		print("Sleeping for 30 seconds")
+		sleep(30)
+
 if __name__ == '__main__':
 	main()
